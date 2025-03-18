da536cd8e70ff852fbfb62aa5d0790c9e44bdfa3
LEPHUCUONG-6A1\Administrator <cuongphu0202@gmail.com>
2025-03-18 11:21:13 +0700

    complete

---
+++ vending_machiness2.srcs/sources_1/new/check_moduless2.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/08/2024 09:35:44 PM
+// Design Name: 
+// Module Name: check_moduless2
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module check_moduless2(
+input clk,
+input [2:0] selected_id,
+input [2:0] selected_quantity,
+input [2:0] inventory_id,
+output reg valid_id,
+output reg valid_quantity
+    );
+always@(posedge clk) begin
+    if(selected_id >= 3'b000 && selected_id <= 3'b111) begin
+        valid_id <= 1;
+    end else begin
+        valid_id <= 0;
+    end
+    if(selected_quantity >= 1 && selected_quantity <= inventory_id) begin
+        valid_quantity <= 1;
+    end else begin
+        valid_quantity <= 0;
+    end
+end
+endmodule
